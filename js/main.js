c6b675521a1428bd1e451a4fc4e1aad709ef0bed
monicawinter <m.m.winter@hotmail.com>
2016-03-15 16:21:49 -0400

    took off unnecessary css and html and added css to js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
-var $html = $('.html');
+var $html = $('html');
+var $body = $('body');
 
 $html.on('keydown', function (e) {
-  var div = document.createElement('div');
+  var div = $('<div>');
+  $body.append(div);
+  div.addClass('ball');
+  div.css('top', Math.random() * 1000);
+  div.css('left', Math.random() * 1000);
 });
-
-$('keydown').on('click', function ()
-)
-
-$('ball').on('click', function (
-  var rand = Math.floor(Math.random();
-))
