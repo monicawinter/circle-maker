f7709dbdd38127f2e218c6548bec743495a2b1c4
monicawinter <m.m.winter@hotmail.com>
2016-03-15 16:02:34 -0400

    started adding content

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var $html = $('.html');
+
+$html.on('keydown', function (e) {
+  var div = document.createElement('div');
+});
+
+$('keydown').on('click', function ()
+)
+
+$('ball').on('click', function (
+  var rand = Math.floor(Math.random();
+))
